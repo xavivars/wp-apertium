bf023d4249600f9adb105fb5a5d5454bef198c61
Xavi Ivars <xavi.ivars@gmail.com>
2008-12-01 09:56:47 +0000

    Afegit locale 'eu'

--- inc/locale.php
+++ inc/locale.php
@@ -2,47 +2,61 @@
 
 	$_names = array();
 
-
-
 	$_names['ca'] = array();
-	$_names['ca']['ca'] = 'català'; 
-	$_names['ca']['es'] = 'espanyol'; 
-	$_names['ca']['en'] = 'anglés'; 
-	$_names['ca']['oc'] = 'occità'; 
-	$_names['ca']['oc@aran'] = 'aranés'; 
-	$_names['ca']['fr'] = 'francés'; 
+	$_names['ca']['ca'] = 'català';
+	$_names['ca']['es'] = 'espanyol';
+	$_names['ca']['eu'] = 'basc';
+	$_names['ca']['en'] = 'anglés';
+	$_names['ca']['oc'] = 'occità';
+	$_names['ca']['oc@aran'] = 'aranés';
+	$_names['ca']['fr'] = 'francés';
 	$_names['ca']['it'] = 'italià';
-	$_names['ca']['translate'] = 'Tradueix aquest text!'; 
-	$_names['ca']['poweredby'] = 'Funciona amb '; 
-	$_names['ca']['translatedto'] = 'Traduit a '; 
-	$_names['ca']['translatedby'] = ' mitjançant '; 
+	$_names['ca']['translate'] = 'Tradueix aquest text!';
+	$_names['ca']['poweredby'] = 'Funciona amb ';
+	$_names['ca']['translatedto'] = 'Traduit a ';
+	$_names['ca']['translatedby'] = ' mitjançant ';
 
 	$_names['en'] = array();
-	$_names['en']['ca'] = 'catalan'; 
-	$_names['en']['es'] = 'spanish'; 
-	$_names['en']['en'] = 'english'; 
-	$_names['en']['oc'] = 'occitan'; 
-	$_names['en']['oc@aran'] = 'aranese'; 
-	$_names['en']['fr'] = 'french'; 
+	$_names['en']['ca'] = 'catalan';
+	$_names['en']['es'] = 'spanish';
+	$_names['en']['eu'] = 'basque';
+	$_names['en']['en'] = 'english';
+	$_names['en']['oc'] = 'occitan';
+	$_names['en']['oc@aran'] = 'aranese';
+	$_names['en']['fr'] = 'french';
 	$_names['en']['it'] = 'italian';
-	$_names['en']['translate'] = 'Translate this text!'; 
-	$_names['en']['poweredby'] = 'Powered by '; 
-	$_names['en']['translatedto'] = 'Translated to '; 
-	$_names['en']['translatedby'] = ' by '; 
-
+	$_names['en']['translate'] = 'Translate this text!';
+	$_names['en']['poweredby'] = 'Powered by ';
+	$_names['en']['translatedto'] = 'Translated to ';
+	$_names['en']['translatedby'] = ' by ';
 
 	$_names['es'] = array();
-	$_names['es']['ca'] = 'catalán'; 
-	$_names['es']['es'] = 'español'; 
-	$_names['es']['en'] = 'inglés'; 
-	$_names['es']['oc'] = 'occitano'; 
-	$_names['es']['oc@aran'] = 'aranés'; 
-	$_names['es']['fr'] = 'francés'; 
+	$_names['es']['ca'] = 'catalán';
+	$_names['es']['eu'] = 'vasco';
+	$_names['es']['es'] = 'español';
+	$_names['es']['en'] = 'inglés';
+	$_names['es']['oc'] = 'occitano';
+	$_names['es']['oc@aran'] = 'aranés';
+	$_names['es']['fr'] = 'francés';
 	$_names['es']['it'] = 'italiano';
-	$_names['es']['translate'] = 'Traduce este texto!'; 
-	$_names['es']['poweredby'] = 'Funciona con '; 
-	$_names['es']['translatedto'] = 'Traducción a '; 
-	$_names['es']['translatedby'] = ' con '; 
+	$_names['es']['translate'] = 'Traduce este texto!';
+	$_names['es']['poweredby'] = 'Funciona con ';
+	$_names['es']['translatedto'] = 'Traducción a ';
+	$_names['es']['translatedby'] = ' con ';
+
+	$_names['eu'] = array();
+	$_names['eu']['ca'] = 'katalana';
+	$_names['eu']['es'] = 'gaztelera';
+	$_names['eu']['eu'] = 'euskara';
+	$_names['eu']['en'] = 'ingelesa';
+	$_names['eu']['oc'] = 'okzitaniera';
+	$_names['eu']['oc@aran'] = 'aranera';
+	$_names['eu']['fr'] = 'frantsesa';
+	$_names['eu']['it'] = 'italiera';
+	$_names['eu']['translate'] = 'Testu hau itzuli!';
+	$_names['eu']['poweredby'] = 'Honekin dabil: ';
+	$_names['eu']['translatedto'] = 'Itzulpena:  ';
+	$_names['eu']['translatedby'] = ' Honekin: ';
 
 	$_names['default'] = $_names['ca'];
 
